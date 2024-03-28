23dd0947bf3a633a6b8ab30e2c1a4fcbd4c5e131
Priyank <144459613+priyankkpatel@users.noreply.github.com>
2024-03-28 11:48:48 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,57 @@
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Register</title>
+</head>
+<body>
+     <style>
+        body{
+            background-image: url('bgim.jpeg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        </style>
+      <div class="container">
+        <div class="box form-box">
+
+        <?php 
+         
+         include("php/config.php");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+         //verifying the unique email
+
+         $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email='$email'");
+
+         if(mysqli_num_rows($verify_query) !=0 ){
+            echo "<div class='message'>
+                      <p>This email is used, Try another One Please!</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO users(Username,Email,Password) VALUES('$username','$email','$password')") or die("Erroe Occured");
+
+            echo "<div class='message'>
+                      <p>Registration successfully!</p>
+                  </div> <br>";
+            echo "<a href='index.php'><button class='btn'>Login Now</button>";
+         
+
+         }
+
+         }else{
+         
+        ?>
+  <style>
 body{
   margin: 0;
   padding: 0;
@@ -98,5 +151,40 @@ input[type="Submit"]{
   width: 100vw;
   height: 25vh;
 }
-  </style>
+</style>
   
+
+<body>
+    <div class="container">
+      <div class="center">
+          <h1>Sign Up</h1>
+          <form method="POST" action="">
+              <div class="txt_field">
+                  <input type="text" name="User name" required>
+                  <span></span>
+                  <label>User Name</label>
+              </div>
+              <div class="txt_field">
+                  <input type="email" name="email" required>
+                  <span></span>
+                  <label>Email</label>
+              </div>
+              <div class="txt_field">
+                  <input type="password" name="password" required>
+                  <span></span>
+                  <label>Password</label>
+              </div>
+              
+              
+              <input name="submit" type="Submit" value="Sign Up">
+              <div class="signup_link">
+                  Have an Account ? <a href="index.html">Login Here</a>
+              </div>
+          </form>
+      </div>
+  </div>
+  </body>
+        <?php } ?>
+      </div>
+</body>
+</html>
